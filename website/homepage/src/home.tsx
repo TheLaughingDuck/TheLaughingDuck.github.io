ff1179b85dfe412840b1c579ccebdb5c42f1c09c
Simon Jorstedt <jorstedtsimon@gmail.com>
2026-08-14 13:54:47 +0200

    Redesigned landing page

--- website/homepage/src/home.tsx
+++ website/homepage/src/home.tsx
@@ -1,11 +1,37 @@
 import Layout from "./navigation"
-
+import hero from "./assets/hero.png"
+import { Link } from "react-router"
 
 export default function Home() {
     return (
         <Layout>
-            <h1>home page</h1>
+            <div className="flex-1 p-6 space-y-4">
+                <div className="flex items-stretch gap-4">
+                    <Link to="aboutme">
+                        <div className="border p-2 h-full rounded-lg bg-gray-100 shadow-md transition-transform duration-200 hover:scale-105 hover:shadow-xl">
+                            <h1 className="font-bold">About me</h1>
+                            <p>I love to build and create, and...</p>
+                        </div>
+                    </Link>
+                    
+                    <div className="border space-fill rounded-lg transition-transform duration-200 hover:scale-105">
+                        <img src={hero} />
+                    </div>
 
+                    <Link to="/projects">
+                        <div className="border p-2 h-full rounded-lg bg-gray-100 shadow-md transition-transform duration-200 hover:scale-105 hover:shadow-xl">
+                            <h1 className="font-bold">Projects</h1>
+
+                            <p>I have worked on...</p>
+                        </div>
+                    </Link>
+
+                </div>
+
+                <div className="border p-2 h-full rounded-lg bg-gray-100 shadow-md transition-transform duration-200 hover:scale-105 hover:shadow-xl">
+                    Hi! I'm Simon, a Data Engineer and Software Developer.
+                </div>
+            </div>
         </Layout>
     )
 }
--- website/homepage/src/navigation.tsx
+++ website/homepage/src/navigation.tsx
@@ -29,29 +29,25 @@ export default function Layout({ children }: { children: ReactNode }) {
     return(
         <div>
             {/* Banner */}
-            <link rel="icon" type="image/png" href="images/DuckHead.png" />
-            <header className="bg-green-600 text-green-900 p-4">
+            <header className="flex bg-blue-200 text-green-900 p-4">
                 <h1 className="text-2x1 font-bold">
                     Hello :)
                 </h1>
+
+                <select className="ml-auto">
+                    <option selected disabled>Settings</option>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
             </header>
             
             {/* Navbar and page content */}
             <div className='flex'>
-                <div className="w-24 bg-gray-200 min-h-screen p-4">
-                    <Navbar />
-                </div>
-
                 {/* Sugpage */}
                 <main className="flex-1 p-6">
                 { children }
                 </main>
             </div>
-
-            <div className='flex'>
-                I built this site myself with React.
-            </div>
-
         </div>
     )
 }
